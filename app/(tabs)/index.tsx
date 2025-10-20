a854dd99be420bb6562ae0fb049960a284ebc986
trevor <trevorgriarte@gmail.com>
2025-10-20 00:15:08 -0700

    more progress

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import ProgressBar  from '../../components/ProgressBar';
 
 export default function App() {
   const [value, setValue] = useState(0);
@@ -8,13 +9,18 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
+      <View style={styles.titleCard}>
+        <Text style={styles.titleText}>ME5A Hours for El Camino College helps you track your time spend in MESA to fulfill your 5 hour requirement each week.</Text>
+      </View>
       <View style={styles.card}>
         <View style={styles.progressCard}>
-          <Text style={styles.progressText}>Progress this week ( ______ )</Text>
+          <Text style={styles.progressText}>Progress this week</Text>
+          <View style={styles.progressBarWrapper}>
+            <ProgressBar/>
+          </View>
         </View>
-        <Text style={styles.timeLeftText}>Time Left: __</Text>
+        <Text style={styles.timeLeftText}>Time Left:</Text>
       </View>
-      
       {/*<View style={styles.progressWrapper}>
         <CircularProgress
           radius={180}
@@ -47,29 +53,53 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#1f419b",
   },
-  card: {
-    marginTop: 30,
-    backgroundColor: '#fff',
+  titleCard: {
+    backgroundColor: '#1f419b',
     borderRadius: 10,
     borderWidth: 5,
-    borderColor: '#ff4539',
+    borderColor: '#fff',
     borderStyle: 'solid',
-    margin: 10,
+    marginTop: 15,
     padding: 10,
     justifyContent: 'center',
     alignItems: 'center',
     width: '90%',
+    height: '15%',
+  },
+  titleText: {
+    padding: 10,
+    borderRadius: 10,
+    borderColor: '#ff4539',
+    color: '#fff',
+    fontWeight: 800,
+    fontSize: 20,
+  },
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    borderWidth: 3,
+    borderColor: '#ff4539',
+    borderStyle: 'solid',
+    marginTop: 15,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '90%',
+    height: '20%',
   },
   progressCard: {
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems:'center',
   },
   progressText: {
     fontWeight: 'bold',
     fontSize: 18,
   },
   timeLeftText: {
-
+    marginTop: 10,
+  },
+  progressBarWrapper: {
+    width: 300,
   }
   /*
   progressWrapper: {
---
+++ components/ProgressBar.tsx
@@ -0,0 +1,70 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Animated, View, StyleSheet, Text } from 'react-native';
+
+const Progress = ({step, steps, height}) => {
+  const animatedValue = React.useRef(new Animated.Value(-1000)).current;
+  const reactive = React.useRef(new Animated.Value(-1000)).current;
+  const [width, setWidth] = React.useState(0);
+
+  React.useEffect(() => {
+    Animated.timing(animatedValue, {
+      toValue: reactive,
+      duration: 300,
+      useNativeDriver: true
+    }).start();
+  }, []);
+
+  React.useEffect(() => {
+    // -width + width * step / steps
+    reactive.setValue(-width + (width * step) / steps);
+  }, [step, width]);
+
+  const progressPercent = Math.round((step / steps) * 100);
+  return (
+    <>
+    <Text style = {{fontFamily: 'Menlo', fontSize: 1, fontWeight: '900', marginBottom: 4}}>
+      {progressPercent}%
+    </Text>
+    <View
+      onLayout={e => {
+          const newWidth = e.nativeEvent.layout.width;
+          setWidth(newWidth);
+      }}
+      style={{
+        height,
+        backgroundColor: 'rgba(0,0,0,0.1)',
+        borderRadius: height,
+        overflow: 'hidden',
+    }}>
+      <Animated.View style={{
+        height,
+        width: '100%',
+        borderRadius: height,
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        position: 'flex',
+        left: 0,
+        top: 0,
+        transform: [{
+          translateX: animatedValue,
+        }]
+      }}/>
+  </View>
+  </>
+  )
+}
+export default function ProgressBar() {
+  return (
+    <View style={styles.container}>
+      <StatusBar hidden />
+      <Progress step={1} steps={5} height={20}/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    padding: 5,
+  }
+})
